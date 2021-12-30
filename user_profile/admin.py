f3832f818c2545f0eeb3c422afa7bccef7828ea5
takuyakubo <takuyakubo2010@gmail.com>
2021-12-31 01:19:17 +0900

    added admin page

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from models import UProfile
 # Register your models here.
+
+
+class UProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UProfile, UProfileAdmin)
